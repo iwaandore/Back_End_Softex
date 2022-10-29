d6075d2afb3088cf499675b406553cd541c59715
André Carlos <95436218+iwaandore@users.noreply.github.com>
2022-10-28 22:53:57 -0300

    Add files via upload

---
+++ lamp/lampada.js
@@ -0,0 +1,23 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+
+function lampOn() {
+
+    lamp.src = './img/ligada.jpg';
+
+}
+
+function lampOff() {
+
+    lamp.src = './img/desligada.jpg';
+
+}
+
+
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
