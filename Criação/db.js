8c2735a16a83d6ec57ca51ec2b0522eab06285c4
André Carlos <95436218+iwaandore@users.noreply.github.com>
2022-11-16 17:07:00 -0300

    Delete Criação directory

--- Criação/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('softex', 'root', '', { dialect: 'mysql', host: 'localhost' });
-
-sequelize.authenticate().then(function () {
-    console.log("conexão realiazda com sucesso")
-}).catch(function (error) {
-    console.log("não está pegando" + error)
-})
