2e318eb2610e8e22c82f3db6b534d0da884dab92
André Carlos <95436218+iwaandore@users.noreply.github.com>
2022-11-23 18:01:01 -0300

    Delete HTML+CSS+JS directory

--- HTML+CSS+JS/cadastrar.js
+++
@@ -1,9 +0,0 @@
-function cadastrar() {
-    var verifica = document.getElementById('data', 'telefone', 'cpf', 'email', 'nome').value
-
-    if (verifica == '') {
-        alert('Error')
-    } else {
-        location.href = 't2.html'
-    }
-}
